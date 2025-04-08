0f9c0f647ccca0714e9f7a736a8e53ec96b4f063
Karmesin924 <jinhong0924@naver.com>
2025-04-08 18:07:27 +0900

    feat: 메인 페이지 - 골프 스윙 이미지 추가

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,21 @@
-import { StyleSheet } from 'react-native';
-import { ThemedText } from '@/components/ThemedText';
+import { ScrollView, StyleSheet } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
 import Header from '@/components/Header';
 import TitleSection from '@/components/home/TitleSection';
 import UploadBox from '@/components/home/UploadBox';
+import GolfImage from '@/components/home/GolfImage';
 
 export default function HomeScreen() {
   return (
     <ThemedView style={styles.container}>
       <Header showUserInfo />
-      <ThemedView style={styles.contents}>
-        <TitleSection />
-        <UploadBox />
-        {/* <GolfImage /> */}
-      </ThemedView>
+      <ScrollView contentContainerStyle={styles.contents}>
+        <ThemedView style={styles.contents}>
+          <TitleSection />
+          <UploadBox />
+          <GolfImage />
+        </ThemedView>
+      </ScrollView>
     </ThemedView>
   );
 }
