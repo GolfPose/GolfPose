039f6ab479465d95948300625116e637d4889567
Karmesin924 <jinhong0924@naver.com>
2025-04-12 01:03:02 +0900

    feat: 공통 폰트 크기 설정

---
+++ constants/Typography.ts
@@ -0,0 +1,14 @@
+import { ms } from 'react-native-size-matters';
+
+const Typography = {
+  xs: ms(12),
+  sm: ms(14),
+  md: ms(16),
+  lg: ms(18),
+  xl: ms(20),
+  '2xl': ms(24),
+  '3xl': ms(28),
+  title: ms(35),
+};
+
+export default Typography;
