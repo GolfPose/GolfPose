542436af5603c2617db7cb0d6eb9ac900af4e484
Karmesin924 <jinhong0924@naver.com>
2025-04-12 09:27:19 +0900

    fix: login.tsx -> props 전달

--- app/login.tsx
+++ app/login.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, useColorScheme } from 'react-native';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import BackHeader from '@/components/BackHeader';
@@ -6,9 +6,11 @@ import TitleSection from '@/components/TitleSection';
 import Typography from '@/constants/Typography';
 
 export default function LoginScreen() {
+  const theme = useColorScheme() as 'light' | 'dark';
+
   return (
     <ThemedView style={styles.container}>
-      <BackHeader />
+      <BackHeader theme={theme} />
       <TitleSection title="로그인" />
     </ThemedView>
   );
