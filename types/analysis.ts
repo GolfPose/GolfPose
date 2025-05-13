121a4022944b2d52148141ff9491853372bce81b
Karmesin924 <jinhong0924@naver.com>
2025-05-13 17:57:57 +0900

    fix: GraphUrl 타입 수정 및 적용

--- types/analysis.ts
+++ types/analysis.ts
@@ -10,16 +10,14 @@ export interface SwingImage {
 }
 
 export interface GraphUrls {
-  leftArm2D?: string;
-  rightArm2D?: string;
-  leftLeg2D?: string;
-  rightLeg2D?: string;
-  main2D?: string;
-  leftArm3D?: string;
-  rightArm3D?: string;
-  leftLeg3D?: string;
-  rightLeg3D?: string;
-  main3D?: string;
+  leftArm2D: string;
+  rightArm2D: string;
+  leftLeg2D: string;
+  rightLeg2D: string;
+  leftArm3D: string;
+  rightArm3D: string;
+  leftLeg3D: string;
+  rightLeg3D: string;
 }
 
 export interface AnalysisRecord {
