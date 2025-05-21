6d1cdde9d3610851ec5d59ebf86385a68f7331fd
ChunSik1230 <goat999@naver.com>
2025-05-21 20:10:56 +0900

    fix: summaryBox제작, 결제금액 위치 수정, 닫기 버튼 다크모드 적용, 신용카드 아이콘 변경, 네이버페이 이미지 변경

--- app/purchase.tsx
+++ app/purchase.tsx
@@ -1,36 +1,47 @@
 import { useLocalSearchParams, useRouter } from 'expo-router';
-import { StyleSheet, Pressable, Image, View, Text } from 'react-native';
+import { StyleSheet, Pressable, Image, View, Text, useColorScheme } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
 import { ThemedText } from '@/components/ThemedText';
 import { Colors } from '@/constants/Colors';
 import Typography from '@/constants/Typography';
 import { s, vs } from 'react-native-size-matters';
 import { Feather } from '@expo/vector-icons';
-
+import { useTheme } from '@/hooks/useTheme';
+import { getColor } from '@/utils/getColor';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function PurchaseScreen() {
   const { title, price } = useLocalSearchParams();
   const router = useRouter();
+  const colorScheme = useColorScheme();
+
+  const theme = useTheme();
+  const iconColor = getColor(theme, {
+    light: Colors.common.black,
+    dark: Colors.common.white,
+  });
 
   return (
     <ThemedView style={styles.container}>
       <ThemedView style={styles.modalHeader}>
         <ThemedText style={styles.modalTitle}>결제수단 선택</ThemedText>
         <Pressable onPress={() => router.back()} style={styles.closeButton}>
-          <Feather name="x" size={s(20)} color="black" />
+          <Feather name="x" size={s(20)} color={iconColor} />
         </Pressable>
       </ThemedView>
 
       <ThemedView style={styles.summaryBox}>
         <ThemedText style={styles.company}>주식회사 자이스웍스</ThemedText>
         <ThemedText style={styles.planTitle}>{title}</ThemedText>
-        <ThemedText style={styles.amountLabel}>결제금액</ThemedText>
-        <ThemedText style={styles.modalPrice}>{price}원</ThemedText>
+        <ThemedView style={styles.priceRow}>
+          <ThemedText style={styles.paymentLabel}>결제금액</ThemedText>
+          <ThemedText style={styles.modalPrice}>{price}원</ThemedText>
+        </ThemedView>
       </ThemedView>
 
       <ThemedText style={styles.sectionTitle}>일반결제</ThemedText>
       <Pressable style={styles.paymentOption}>
-        <Image source={require('@/assets/images/card.png')} style={styles.icon} />
+        <FontAwesome name="credit-card" size={24} color= {iconColor} style={{ marginRight: 12 }} />
         <ThemedText style={styles.paymentText}>신용카드</ThemedText>
       </Pressable>
 
@@ -41,7 +52,7 @@ export default function PurchaseScreen() {
           <ThemedText style={styles.paymentText}>카카오페이</ThemedText>
         </Pressable>
         <Pressable style={styles.quickOption}>
-          <Image source={require('@/assets/images/naver.png')} style={styles.icon} resizeMode="contain" />
+          <Image source={require('@/assets/images/Naver.png')} style={styles.icon} resizeMode="contain" />
           <ThemedText style={styles.paymentText}>네이버페이</ThemedText>
         </Pressable>
       </ThemedView>
@@ -53,7 +64,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: s(20),
-    backgroundColor: Colors.light.background,
   },
   modalHeader: {
     flexDirection: 'row',
@@ -72,9 +82,15 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
   },
   summaryBox: {
-    backgroundColor: Colors.common.white,
-    padding: s(16),
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    padding: s(12),
     borderRadius: s(8),
+    borderWidth: 1,
+    width: s(302),
+    borderColor: Colors.common.gray300,
+    marginTop: vs(8),
     marginBottom: vs(16),
   },
   company: {
@@ -143,4 +159,17 @@ const styles = StyleSheet.create({
     right: s(10),
     top: vs(10),
   },
+  priceRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: s(8),
+    width: s(280),
+    borderColor: Colors.common.gray300,
+  },
+  paymentLabel: {
+    fontSize: Typography.sm,
+    color: Colors.common.gray600,
+  },
+
 });
